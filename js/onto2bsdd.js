6379b84f6d2e5a7056557e66d2c0152694bf5665
jbrouwerdigibase <143397320+jbrouwerdigibase@users.noreply.github.com>
2024-09-20 15:38:56 +0200

    Add conversion fixes and new ketenstandaard demo dictionaries
    * Conversion fixes and new ketenstandaard demo dictionaries
    
    * Fix missing comma in header content
    
    * Increase textarea height to match initial content
    
    * Revert change classProperty code simplification
    
    * bimbasisproducten sparql update quint
    
    * move namespace replacement back to sparql
    
    * add namespace check for OwnedUri
    
    * add additional sparql uri replacements
    
    * Revert creation of redundant variables
    
    * update class docs
    
    * improve bSDD code generation
    
    * update initial header
    
    * fix constructing OwnedUri
    
    * use property name as code to align with IFC mapping rules
    
    * improve classProperty code generation
    
    * add related IFC entity names
    
    * improve classProperty code generation
    
    * format generated code as uuid
    
    * update samples
    
    * move uid from classProperty to property
    
    * update samples with correct uid

--- js/onto2bsdd.js
+++ js/onto2bsdd.js
@@ -1,29 +1,38 @@
 /* SPDX-License-Identifier: Apache-2.0
    Copyright © 2023 Sander Stolk */
 
+const BSDD_NAMESPACE = "https://identifier.buildingsmart.org/";
+const BSDD_IFC_NAMESPACE =
+  "https://identifier.buildingsmart.org/uri/buildingsmart/ifc";
+
 class Onto2bsdd {
   /* Transforms input CSV content to bSDD in its JSON import model format.
-       The CSV content is expected to have a header with the following columns:
-       - ontoClassPrefLabel
-       - ontoClassURI
-       - ontoClassDefinition
-       - ifcClassLabel
-       - ifcClassURI
-       - ontoParentClassPrefLabel
-       - ontoParentClass
-       - ontoPropertyURI
-       - ontoPropertyPrefLabel
-       - ontoPropertyDatatypeLabel
-       - ontoPropertyDatatype
-       
-       For documentation on the bSDD import model, see: 
-       https://github.com/buildingSMART/bSDD/blob/master/Documentation/bSDD%20JSON%20import%20model.md .
-    */
+            The CSV content is expected to have a header with the following columns:
+            - ontoClassPrefLabel
+            - ontoClassURI
+            - ontoClassDefinition
+            - mappedClassRelation
+            - mappedClassRelationURI (unused)
+            - mappedClassURI
+            - ontoParentClassPrefLabel (unused)
+            - ontoParentClass
+            - ontoPropertyURI
+            - ontoPropertyPrefLabel
+            - ontoPropertyDatatypeLabel
+            - ontoPropertyDatatype         
+   
+            For documentation on the bSDD import model, see: 
+            https://github.com/buildingSMART/bSDD/blob/master/Documentation/bSDD%20JSON%20import%20model.md .
+         */
   static fromCSV(input, header = {}) {
     const csvObjects = $.csv.toObjects(input); // see https://github.com/evanplaice/jquery-csv/#documentation
     console.log(JSON.stringify(csvObjects));
 
     const result = JSON.parse(JSON.stringify(header));
+
+    // // set release date to now
+    // result.ReleaseDate = new Date().toISOString();
+
     const dictionaryUri = result.DictionaryUri;
     const resultClassifications = [];
     const resultProperties = [];
@@ -39,18 +48,23 @@ class Onto2bsdd {
       if (!resultClassificationObject) {
         resultClassificationObject = {
           ClassType: "Class",
-          Code: Onto2bsdd.getLocalname(csvObject.ontoClassURI),
+          Code: Onto2bsdd.codeFromName(
+            Onto2bsdd.getLocalname(csvObject.ontoClassURI)
+          ),
           Definition: csvObject.ontoClassDefinition,
           Name: csvObject.ontoClassPrefLabel,
           OwnedUri: csvObject.ontoClassURI,
-          ParentClassificationCode: Onto2bsdd.getLocalname(
-            csvObject.ontoParentClass
-          ),
-          // RelatedIfcEntityNamesList: [csvObject.ifcClassLabel],
           Status: "Preview",
           ClassRelations: [],
           ClassProperties: [],
         };
+
+        if (csvObject.ontoParentClass) {
+          resultClassificationObject.ParentClassCode = Onto2bsdd.codeFromName(
+            Onto2bsdd.getLocalname(csvObject.ontoParentClass)
+          );
+        }
+
         resultClassifications.push(resultClassificationObject);
       }
 
@@ -68,58 +82,76 @@ class Onto2bsdd {
               ? "Time"
               : "String";
           resultPropertyObject = {
-            Code: Onto2bsdd.getLocalname(csvObject.ontoPropertyURI),
-            DataType: bsddDatatype,
-            Definition: csvObject.ontoPropertyDefinition,
+            Code: Onto2bsdd.codeFromName(csvObject.ontoPropertyPrefLabel),
             Name: csvObject.ontoPropertyPrefLabel,
+            Definition: csvObject.ontoPropertyDefinition,
+            DataType: bsddDatatype,
             OwnedUri: csvObject.ontoPropertyURI,
+            Uid: Onto2bsdd.getLocalname(
+              csvObject.ontoPropertyURI
+            ),
           };
           resultProperties.push(resultPropertyObject);
         }
-
-        const resultPropertyLinkObject = {
-          Code: md5(
-            resultClassificationObject.Code + "-" + resultPropertyObject.Code
-          ),
+        const classPropertyCode = Onto2bsdd.generateFormattedUUID(
+          resultClassificationObject.Code,
+          resultPropertyObject.Uid
+        );
+        const classProperty = {
+          Code: classPropertyCode,
           PropertyCode: resultPropertyObject.Code,
           PropertySet: result.DictionaryCode,
           PropertyType: "Property",
         };
-        if (csvObject.ontoPropertyURI.includes(dictionaryUri)) {
-          resultPropertyLinkObject.OwnedUri = csvObject.ontoPropertyURI;
+        if (Onto2bsdd.isInBsddNamespace(csvObject.ontoPropertyURI)) {
+          classProperty.OwnedUri = Onto2bsdd.combineUris(
+            resultPropertyObject.Code,
+            csvObject.ontoPropertyURI,
+            "prop"
+          );
         } else {
-          resultPropertyLinkObject.OwnedUri = dictionaryUri + "fake-uri-404";
+          classProperty.OwnedUri = Onto2bsdd.combineUris(
+            classPropertyCode,
+            csvObject.ontoPropertyURI,
+            "prop"
+          );
         }
-        resultClassificationObject.ClassProperties.push(
-          resultPropertyLinkObject
-        );
+        resultClassificationObject.ClassProperties.push(classProperty);
       }
 
-      if (csvObject.mappedClassURI && csvObject.mappedClassRelation) {
-        const resultIfcRelationObject = {
+      // Only add class relations if the related class is in the bSDD namespace
+      // because we don't know the URI of the related class on bSDD
+      // if we do have that information we can also add the OwnedUri property
+      if (
+        Onto2bsdd.isInBsddNamespace(csvObject.mappedClassURI) &&
+        csvObject.mappedClassRelation
+      ) {
+        const classRelation = {
           RelationType: csvObject.mappedClassRelation,
           RelatedClassUri: csvObject.mappedClassURI,
         };
-        if (csvObject.mappedClassURI.includes(dictionaryUri)) {
-          resultIfcRelationObject.OwnedUri = csvObject.mappedClassURI;
-        } else {
-          resultIfcRelationObject.OwnedUri = dictionaryUri + "fake-uri-404";
-        }
 
-        // switch (csvObject.mappedClassRelation) {
-        //   case "isSpecializationOf":
-        //     resultIfcRelationObject.RelationType = "IsChildOf";
-        //     break;
-        //   case "isGeneralizationOf":
-        //     resultIfcRelationObject.RelationType = "IsParentOf";
-        //     break;
-        //   case "isRelatedTo":
-        //     resultIfcRelationObject.RelationType = "HasReference";
-        //     break;
-        //   default:
-        //     break;
-        // }
-        resultClassificationObject.ClassRelations.push(resultIfcRelationObject);
+        if (Onto2bsdd.isInBsddNamespace(csvObject.mappedClassURI)) {
+          classRelation.OwnedUri = Onto2bsdd.combineUris(
+            csvObject.mappedClassURI,
+            csvObject.ontoClassURI,
+            "relclass"
+          );
+        } else {
+          classRelation.OwnedUri = csvObject.mappedClassURI;
+        }
+        resultClassificationObject.ClassRelations.push(classRelation);
+      }
+
+      // Add related IFC entity names to the classification object
+      const relatedIfcEntityNamesList =
+        resultClassificationObject.ClassRelations.filter((relation) =>
+          relation.RelatedClassUri.startsWith(BSDD_IFC_NAMESPACE)
+        ).map((relation) => relation.RelatedClassUri.split("/").pop());
+
+      if (relatedIfcEntityNamesList.length > 0) {
+        resultClassificationObject.RelatedIfcEntityNamesList =
+          relatedIfcEntityNamesList;
       }
     }
 
@@ -131,9 +163,111 @@ class Onto2bsdd {
     return JSON.stringify(result, null, 2);
   }
 
+  /**
+   * Generates a formatted UUID based on the MD5 hash of the concatenation
+   * of the classification code and property URI.
+   *
+   * @param {string} classificationCode - The code of the classification object.
+   * @param {string} propertyUri - The URI of the property.
+   * @returns {string} The formatted UUID.
+   * @throws {TypeError} If the input parameters are not strings.
+   */
+  static generateFormattedUUID(classificationCode, propertyUri) {
+    if (
+      typeof classificationCode !== "string" ||
+      typeof propertyUri !== "string"
+    ) {
+      throw new TypeError(
+        "Both classificationCode and propertyUri must be strings"
+      );
+    }
+
+    // Concatenate classificationCode and propertyUri with a hyphen
+    const concatenatedString = `${classificationCode}-${propertyUri}`;
+
+    // Generate the MD5 hash of the concatenated string
+    const hash = md5(concatenatedString);
+
+    // Format hash as a UUID
+    const formattedUUID = `${hash.slice(0, 8)}-${hash.slice(
+      8,
+      12
+    )}-${hash.slice(12, 16)}-${hash.slice(16, 20)}-${hash.slice(20)}`;
+
+    return formattedUUID;
+  }
+
+  /**
+   * Combines a base URI with another URI and a specified URI type segment.
+   * It cleans the URI and constructs a combined URI.
+   *
+   * @param {string} uri - The URI to be appended.
+   * @param {string} baseUri - The base URI to which the other URI will be appended.
+   * @param {string} [uriType='relclass'] - The URI type segment that links both URIs.
+   * @returns {string} - The combined URI.
+   */
+  static combineUris(uri, baseUri, uriType = "relclass") {
+    const cleanedUri = uri
+      .replace(BSDD_NAMESPACE, "")
+      .replace(/^https?:\/\//, "");
+
+    const combinedUri = [baseUri, uriType, cleanedUri]
+      .join("/")
+      .replace(/\/{2,}/g, "/")
+      .replace(":/", "://");
+
+    return combinedUri;
+  }
+
+  /**
+   * Converts a string into a valid bSDD code.
+   *
+   * The function replaces spaces with underscores and removes any characters
+   * that are not alphanumeric, diacritics, underscores, hyphens, dots, or whitespace.
+   *
+   * @param {string} name - The string to convert into a valid bSDD code.
+   * @returns {string} The converted bSDD code.
+   * @throws {TypeError} If the input is not a string.
+   */
+  static codeFromName(name) {
+    if (typeof name !== "string") {
+      throw new TypeError("Input must be a string");
+    }
+
+    let code = name.replace(/ /g, "_");
+    const allowedChars = /[^a-zA-Z0-9\s._-À-ž]/g;
+    code = code.replace(allowedChars, "");
+
+    return code;
+  }
+
+  /**
+   * Checks if a given URI is within the buildingsmart namespace.
+   *
+   * @param {string} uri - The URI to check.
+   * @returns {boolean} - Returns true if the URI starts with BSDD_NAMESPACE, otherwise false.
+   */
+  static isInBsddNamespace(uri) {
+    if (typeof uri !== "string") return false;
+    return uri.startsWith(BSDD_NAMESPACE);
+  }
+
+  /**
+   * Extracts the local name from a given URI.
+   *
+   * The local name is the part of the URI after the last '#' or '/' character.
+   *
+   * @param {string} uri - The URI from which to extract the local name.
+   * @returns {string|null} The local name extracted from the URI, or null if the input is not a string.
+   */
   static getLocalname(uri) {
-    if (uri.includes("#")) return uri.split("#").pop();
-    return uri.split("/").pop();
+    if (typeof uri !== "string") return null;
+
+    const trimmedUri = uri.trim();
+    const delimiter = trimmedUri.includes("#") ? "#" : "/";
+    const parts = trimmedUri.split(delimiter);
+
+    return parts.pop() || null;
   }
 
   static getObjectWithProperty(containerObject, propertyName, propertyValue) {
@@ -147,12 +281,9 @@ class Onto2bsdd {
   static pruneInternalReferences(result) {
     for (const classification of result.Classes) {
       if (
-        !Onto2bsdd.isPresent(
-          classification.ParentClassificationCode,
-          result.Classes
-        )
+        !Onto2bsdd.isPresent(classification.ParentClassCode, result.Classes)
       ) {
-        classification.ParentClassificationCode = undefined;
+        classification.ParentClassCode = undefined;
       }
       for (const classificationProperty of classification.ClassProperties) {
         if (
